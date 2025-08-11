6fe1ef18e333ec4360cde7b34fa411d35c1c9ed9
Alane Dantas <alane.programming1@gmail.com>
2025-08-11 08:38:50 -0300

    11/08/2025

---
+++ Spring/projetodaweb/projetodaweb/src/main/java/br/edu/ifpb/daweb/elenilson/projetodaweb/business/dto/error.java
@@ -0,0 +1,7 @@
+package br.edu.ifpb.daweb.elenilson.projetodaweb.business.dto;
+
+public class error {
+	public static void main(String[] args) {
+	System.out.println("Testando essa loucura muito louca");
+	}
+}
